0ac35c66faae47ec99aa8acc6230f1d6329620e6
chenhantao <hantaochen@alog.com>
2019-01-03 16:55:54 +0800

    小修改

--- springbootDemo-web/src/main/java/com/cht/Application.java
+++ springbootDemo-web/src/main/java/com/cht/Application.java
@@ -1,9 +1,15 @@
 package com.cht;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * @author chenhantao
  * @since 2018/11/30
  */
-
+@SpringBootApplication
 public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+    }
 }
