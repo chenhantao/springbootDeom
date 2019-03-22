1fa3b06283a18fb59152ac0de8c0e63613f79fba
chenhantao <hantaochen@alog.com>
2019-03-22 14:41:40 +0800

    test3

---
+++ demo/src/main/java/cht/CDPlayerConfig.java
@@ -0,0 +1,15 @@
+package cht;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * TODO
+ *
+ * @author chenhantao
+ * @since 2019/3/22
+ */
+@Configuration
+@ComponentScan
+public class CDPlayerConfig {
+}
---
+++ demo/src/test/java/cht/CDPlayerTest.java
@@ -0,0 +1,32 @@
+package cht;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * TODO
+ *
+ * @author chenhantao
+ * @since 2019/3/22
+ */
+@SpringBootTest
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = CDPlayerConfig.class)
+public class CDPlayerTest {
+
+    @Autowired
+    private CompactDisc cd;
+
+    @Test
+    public void cdShouldNotBeNull() {
+        if (cd == null) {
+            System.out.println("null");
+        } else {
+            System.out.println("not null");
+        }
+    }
+}
